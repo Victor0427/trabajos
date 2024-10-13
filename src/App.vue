a4b0659698050cbb00d9cba3446aa65b3cc161c5
Victor0427 <vleonpatron4@gmail.com>
2024-10-13 15:45:21 -0500

    trabajos

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<!-- src/App.vue -->
+<template>
+  <div id="app">
+    <ColorSelector />
+  </div>
+</template>
+
+<script>
+import ColorSelector from './pages/ColorSelector.vue';
+
+export default {
+  components: {
+    ColorSelector,
+  },
+};
+</script>
---
+++ src/pages/ColorSelector.vue
@@ -0,0 +1,27 @@
+
+<template>
+    <div>
+      <label for="color">Selecciona un color:</label>
+      <select v-model="selectedColor">
+        <option value="gray">Gris</option>
+        <option value="red">Rojo</option>
+        <option value="yellow">Amarillo</option>
+        <option value="green">Verde</option>
+      </select>
+  
+      <TrafficLight :color="selectedColor" />
+    </div>
+  </template>
+  
+  <script>
+  import TrafficLight from '@/components/TrafficLight.vue';
+  
+  export default {
+    components: { TrafficLight },
+    data() {
+      return {
+        selectedColor: 'gray',
+      };
+    },
+  };
+  </script>
